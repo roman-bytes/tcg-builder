1c39f1e24d20185fe2643439b29e9b573e350c6c
Jacob Roman <jcbrmn06@gmail.com>
2024-12-14 07:38:50 -0500

    added basic readme

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# POKEMON PICK 6 SERVER
+
+This is a go server that uses the [PokemonTCG SDK](https://github.com/PokemonTCG/pokemon-tcg-sdk-go-v2) to fetch a random card from the Pokemon TCG API.
+
+You will need to set the `POKEMON_API_KEY` environment variable to your Pokemon TCG API key. Sign up for a free API key [here](https://pokemontcg.io/).
+
+You can run the server using `go run main.go`
--- server/main.go
+++ server/main.go
@@ -43,68 +43,67 @@ func setupApp() *fiber.App {
 
 
 func storeCard(c *fiber.Ctx) error {
+	var card Card
+	if err := c.BodyParser(&card); err != nil {
+		return c.Status(400).SendString("Invalid request")
+	}
 
-		var card Card
-		if err := c.BodyParser(&card); err != nil {
-			return c.Status(400).SendString("Invalid request")
-		}
-
-		// Get the existing cookie
-		existingCookie := c.Cookies("storedCards")
-		var selectedCards []Card
+	// Get the existing cookie
+	existingCookie := c.Cookies("storedCards")
+	var selectedCards []Card
 		
-		// Only unmarshal if the cookie is not empty
-		if existingCookie != "" {
-			if err := json.Unmarshal([]byte(existingCookie), &selectedCards); err != nil {
-				return c.Status(500).SendString("Error unmarshalling cookie")
-			}
+	// Only unmarshal if the cookie is not empty
+	if existingCookie != "" {
+		if err := json.Unmarshal([]byte(existingCookie), &selectedCards); err != nil {
+			return c.Status(500).SendString("Error unmarshalling cookie")
 		}
+	}
 
-		// Add the new card if its not already in the cookie
-		for _, storedCard := range selectedCards {
-			if storedCard.ID == card.ID {
-				return c.SendString("Card already stored")
-			}
+	// Add the new card if its not already in the cookie
+	for _, storedCard := range selectedCards {
+		if storedCard.ID == card.ID {
+			return c.SendString("Card already stored")
 		}
+	}
 
-		// Check if the limit of 6 cards is reached
-		if len(selectedCards) >= 6 {
-			return c.Status(400).SendString("Limit of 6 cards reached")
-		}
+	// Check if the limit of 6 cards is reached
+	if len(selectedCards) >= 6 {
+		return c.Status(400).SendString("Limit of 6 cards reached")
+	}
 
-		// Add the new card
-		selectedCards = append(selectedCards, card)
+	// Add the new card
+	selectedCards = append(selectedCards, card)
 
-		// Covert back to JSON and update the cookie
-		updatedCookie, err := json.Marshal(selectedCards)
-		if err != nil {
-			return c.Status(500).SendString("Error marshalling cookie")
-		}
+	// Covert back to JSON and update the cookie
+	updatedCookie, err := json.Marshal(selectedCards)
+	if err != nil {
+		return c.Status(500).SendString("Error marshalling cookie")
+	}
 
-		c.Cookie(&fiber.Cookie{
-			Name: "storedCards",
-			Value: string(updatedCookie),
-			Expires: time.Now().Add(time.Hour * 24),
-		})
+	c.Cookie(&fiber.Cookie{
+		Name: "storedCards",
+		Value: string(updatedCookie),
+		Expires: time.Now().Add(time.Hour * 24),
+	})
 
-		return c.Status(200).SendString("Card stored successfully")
+	return c.Status(200).SendString("Card stored successfully")
 }
 
 func getStoredCards(c *fiber.Ctx) error {
 
-		// Get cookie
-		existingCookie := c.Cookies("storedCards")
-		var selectedCards []Card
-		
-		if existingCookie == "" {
-			return c.JSON([]Card{})
-		}
+	// Get cookie
+	existingCookie := c.Cookies("storedCards")
+	var selectedCards []Card
+	
+	if existingCookie == "" {
+		return c.JSON([]Card{})
+	}
 
-		if err := json.Unmarshal([]byte(existingCookie), &selectedCards); err != nil {
-			return c.Status(500).SendString("Error unmarshalling cookie")
-		}
+	if err := json.Unmarshal([]byte(existingCookie), &selectedCards); err != nil {
+		return c.Status(500).SendString("Error unmarshalling cookie")
+	}
 
-		return c.JSON(selectedCards)
+	return c.JSON(selectedCards)
 }
 
 func getRandomCard(c *fiber.Ctx) error {
@@ -118,16 +117,16 @@ func getRandomCard(c *fiber.Ctx) error {
   	cardClient := tcg.NewClient(apiKey)
 	tcgTotalCards := 18506
 	randomPage := rand.Intn((tcgTotalCards / 250) + 1) 
-		cards, err := cardClient.GetCards(
-			request.Page(randomPage),
-			request.PageSize(1), // Needs to stay 1 so that we only get one card back
-		)
+	cards, err := cardClient.GetCards(
+		request.Page(randomPage),
+		request.PageSize(1), // Needs to stay 1 so that we only get one card back
+	)
 
-		if err != nil {
-			log.Fatal(err)
-		}
+	if err != nil {
+		log.Fatal(err)
+			}
 
-		return c.JSON(cards);
+	return c.JSON(cards);
 }
 
 
