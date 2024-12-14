6fa381fadf0e4c3d13eece3ced352f2e62933a1a
Jacob Roman <jcbrmn06@gmail.com>
2024-12-14 08:18:07 -0500

    added API services

---
+++ app/src/app/api.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  //TODO: update for production. ENV?
+  private baseURL = 'http://localhost:3000';
+
+  constructor() { }
+
+  //** Get a random card */
+  getRandomCard(): Promise<any> {
+    return fetch(`${this.baseURL}/random-card`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Could not fetch random card');
+        }
+        return response.json();
+      });
+  }
+
+  //** Store a card */
+  storeCard(card: any): Promise<any> {
+    return fetch(`${this.baseURL}/store`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(card)
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error('Could not store card');
+      }
+      return response.json();
+    });
+  }
+
+  //** Get stored cards */
+  getStoredCards(): Promise<any> {
+    return fetch(`${this.baseURL}/stored`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Could not fetch stored cards');
+        }
+        return response.json();
+      });
+  }
+}
